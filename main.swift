efdce32a09fd665feaa2b6fb7bd5bd9a57e2176b
Waihon Yew <yewwaihon@gmail.com>
2022-04-03 05:34:48 +0000

    Initial commit

---
+++ main.swift
@@ -0,0 +1,8 @@
+func checkpoint01() {
+    let celsius = 36.9
+    let fahrenheit = celsius * 9.0 / 5.0 + 32.0
+    print("Celsius: \(celsius)")
+    print("Fahrenheit: \(fahrenheit)")
+}
+
+checkpoint01()
