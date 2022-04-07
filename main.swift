48d8956c37063b2c8ce7026ce6525277add0030d
Waihon Yew <yewwaihon@gmail.com>
2022-04-07 15:48:19 +0000

    Fizz Buzz coding challenge

--- main.swift
+++ main.swift
@@ -1,3 +1,5 @@
+import Foundation
+
 func checkpoint01() {
     let celsius = 36.9
     let fahrenheit = celsius * 9.0 / 5.0 + 32.0
@@ -15,3 +17,19 @@ func checkpoint02() {
 }
 
 checkpoint02()
+
+func checkpoint03() {
+    for i in 1...100 {
+        if i % 3 == 0 && i % 5 == 0 {
+            print("FizzBuzz")
+        } else if i % 3 == 0 {
+            print("Fizz")
+        } else if i % 5 == 0 {
+            print("Buzz")
+        } else {
+            print(i)
+        }
+    }
+}
+
+checkpoint03()
